06c0ddc6c96ed1ec91112c42ac0e8ed1bc287a1b
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2024-07-06 05:45:34 +0500

    uhhk

--- lib/app_provider.dart
+++ lib/app_provider.dart
@@ -10,6 +10,7 @@ import 'package:ieltszone/pages/test_result.dart';
 import 'package:ieltszone/services/storage_service.dart';
 
 const String url = 'http://192.168.93.65:8000';
+// const String url = 'https://ieltszone.pythonanywhere.com';
 
 class AppProvider extends ChangeNotifier {
   final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
@@ -51,16 +52,50 @@ class AppProvider extends ChangeNotifier {
   }
 
   void nextTest() {
-    currentTestIndex++;
-
-    if (currentTestIndex >= exam.tests.length) {
-      currentTestIndex = 0;
-
-      navigatorKey.currentState!.pushReplacement(MaterialPageRoute(builder: (context) => const ResultPage()));
-      timer.cancel();
+    if (currentTestIndex < exam.tests.length - 1) {
+      currentTestIndex += 1;
+      notifyListeners();
     }
+  }
 
-    notifyListeners();
+  void previousTest() {
+    if (currentTestIndex > 0) {
+      currentTestIndex -= 1;
+      notifyListeners();
+    }
+  }
+
+  void finishTest() async {
+    navigatorKey.currentState!.pop();
+
+    showDialog(
+      context: navigatorKey.currentContext!,
+      builder: (context) => Center(
+        child: CircularProgressIndicator(
+          color: Theme.of(navigatorKey.currentContext!).colorScheme.secondary,
+        ),
+      ),
+    );
+
+    Map data = {
+      'exam': exam.id.toString(),
+      'answers': exam.tests
+          .map((e) => {
+                "test": e.id.toString(),
+                "answer": e.answerId.toString(),
+              })
+          .toList(),
+    };
+
+    User user = UserStorage().getUser()!;
+
+    Response response = await post(Uri.parse('$url/api/answer-sheet/'), body: jsonEncode(data), headers: {'Authorization': 'Token ${user.token}', 'Content-Type': 'application/json; charset=utf-8'});
+
+    navigatorKey.currentState!.pop();
+
+    if (response.statusCode == 201) {
+      navigatorKey.currentState!.pushReplacement(MaterialPageRoute(builder: (context) => const ResultPage()));
+    }
   }
 
   void loadUser() {
@@ -76,19 +111,40 @@ class AppProvider extends ChangeNotifier {
 
   login(int id, String password) async => await post(Uri.parse('$url/api/login/'), body: {'id': id.toString(), 'password': password});
 
-  void fetchExam(String code) async {
+  fetchExam(String code) async {
     if (code.trim() != '') {
+      showDialog(
+        context: navigatorKey.currentContext!,
+        builder: (context) => Center(
+          child: CircularProgressIndicator(
+            color: Theme.of(navigatorKey.currentContext!).colorScheme.secondary,
+          ),
+        ),
+      );
+
       Response response = await get(Uri.parse('$url/api/exams/by-code/$code/'));
 
       if (response.statusCode == 200) {
         Exam exam = Exam.fromJson(jsonDecode(response.body));
 
         this.exam = exam;
+        currentTestIndex = 0;
 
         notifyListeners();
 
-        navigatorKey.currentState!.push(MaterialPageRoute(builder: (context) => const TestEntryPage()));
+        navigatorKey.currentState!.pop();
+        return navigatorKey.currentState!.push(MaterialPageRoute(builder: (context) => const TestEntryPage()));
       }
+
+      navigatorKey.currentState!.pop();
+    }
+  }
+
+  void setTestByIndex(int index) {
+    if (index >= 0 && index <= exam.tests.length - 1) {
+      currentTestIndex = index;
+
+      notifyListeners();
     }
   }
 }
--- lib/pages/solve_test.dart
+++ lib/pages/solve_test.dart
@@ -35,101 +35,208 @@ class _SolveTestState extends State<SolveTest> with WidgetsBindingObserver {
   Widget build(BuildContext context) {
     AppProvider provider = Provider.of(context, listen: true);
 
-    double width = MediaQuery.of(context).size.width * 0.8 * (provider.timeLeft / provider.exam.duration);
+    double width = MediaQuery.of(context).size.width * 0.9 * (provider.timeLeft / provider.exam.duration);
+
+    bool previousBtn = provider.currentTestIndex > 0;
+    bool finishBtn = provider.process == 1.0;
+    bool nextBtn = provider.currentTestIndex < provider.exam.tests.length - 1;
 
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
       body: SafeArea(
-        child: Stack(
-          children: [
-            Center(
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+        child: Center(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    Expanded(
-                      flex: 3,
-                      child: Center(
-                        child: Text(
-                          provider.currentTest.question,
-                          maxLines: 2,
-                          style: TextStyle(color: Theme.of(context).colorScheme.secondary, fontSize: 20, fontWeight: FontWeight.bold),
-                          overflow: TextOverflow.ellipsis,
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 6,
-                      child: Column(
-                        children: [
-                          const SizedBox(height: 50),
-                          ...getAnswers(),
-                          const SizedBox(height: 30),
-                          InkWell(
-                            onTap: provider.nextTest,
-                            child: Ink(
-                              padding: const EdgeInsets.all(12),
-                              width: double.maxFinite,
-                              decoration: BoxDecoration(
-                                color: Theme.of(context).colorScheme.secondary,
-                                borderRadius: BorderRadius.circular(4),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  'Next',
-                                  style: TextStyle(color: Theme.of(context).colorScheme.primary, fontSize: 16, fontWeight: FontWeight.w700),
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    secondsToTime(provider.timeLeft),
-                    style: TextStyle(color: Theme.of(context).colorScheme.secondary),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 4),
-                    width: width,
-                    height: 8,
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.secondary,
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 12),
-                    width: MediaQuery.of(context).size.width * 0.8,
-                    height: 8,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      border: Border.all(width: 1, color: Theme.of(context).colorScheme.secondary),
-                    ),
-                    alignment: Alignment.centerLeft,
-                    child: Container(
-                      width: MediaQuery.of(context).size.width * 0.8 * provider.process,
+                    Container(
+                      margin: const EdgeInsets.only(top: 4),
+                      width: width,
+                      height: 8,
                       decoration: BoxDecoration(
                         color: Theme.of(context).colorScheme.secondary,
                         borderRadius: BorderRadius.circular(50),
                       ),
                     ),
+                    Text(
+                      secondsToTime(provider.timeLeft),
+                      style: TextStyle(color: Theme.of(context).colorScheme.secondary),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        flex: 3,
+                        child: Center(
+                          child: Text(
+                            provider.currentTest.question,
+                            maxLines: 2,
+                            style: TextStyle(color: Theme.of(context).colorScheme.secondary, fontSize: 20, fontWeight: FontWeight.bold),
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 6,
+                        child: Column(
+                          children: [
+                            const SizedBox(height: 50),
+                            ...getAnswers(),
+                            const SizedBox(height: 30),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: InkWell(
+                                    borderRadius: BorderRadius.circular(4),
+                                    onTap: provider.previousTest,
+                                    child: Ink(
+                                      padding: const EdgeInsets.all(10),
+                                      width: double.maxFinite,
+                                      decoration: BoxDecoration(
+                                        color: previousBtn ? Theme.of(context).colorScheme.secondary : null,
+                                        borderRadius: BorderRadius.circular(4),
+                                        border: Border.all(width: 1, color: Theme.of(context).colorScheme.secondary),
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          'Previous',
+                                          style: TextStyle(
+                                            color: previousBtn ? Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.secondary,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w700,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Expanded(
+                                  child: InkWell(
+                                    borderRadius: BorderRadius.circular(4),
+                                    onTap: () => finishBtn
+                                        ? showDialog(
+                                            context: context,
+                                            builder: (contex) => AlertDialog(
+                                              title: const Text('Are you sure ?'),
+                                              actions: [
+                                                MaterialButton(
+                                                  onPressed: () => Navigator.pop(context),
+                                                  child: const Text('Cancel'),
+                                                ),
+                                                MaterialButton(
+                                                  onPressed: () => provider.finishTest(),
+                                                  textColor: Theme.of(context).colorScheme.secondary,
+                                                  color: Theme.of(context).colorScheme.primary,
+                                                  child: const Text('Finish'),
+                                                )
+                                              ],
+                                            ),
+                                          )
+                                        : null,
+                                    child: Ink(
+                                      padding: const EdgeInsets.all(10),
+                                      width: double.maxFinite,
+                                      decoration: BoxDecoration(
+                                        color: finishBtn ? Theme.of(context).colorScheme.secondary : null,
+                                        borderRadius: BorderRadius.circular(4),
+                                        border: Border.all(width: 1, color: Theme.of(context).colorScheme.secondary),
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          'Finish',
+                                          style: TextStyle(
+                                            color: finishBtn ? Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.secondary,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w700,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(width: 10),
+                                Expanded(
+                                  child: InkWell(
+                                    borderRadius: BorderRadius.circular(4),
+                                    onTap: provider.nextTest,
+                                    child: Ink(
+                                      padding: const EdgeInsets.all(10),
+                                      width: double.maxFinite,
+                                      decoration: BoxDecoration(
+                                        color: nextBtn ? Theme.of(context).colorScheme.secondary : null,
+                                        borderRadius: BorderRadius.circular(4),
+                                        border: Border.all(width: 1, color: Theme.of(context).colorScheme.secondary),
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          'Next',
+                                          style: TextStyle(
+                                            color: nextBtn ? Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.secondary,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w700,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      )
+                    ],
                   ),
-                ],
-              ),
-            )
-          ],
+                ),
+                SizedBox(
+                  height: 52,
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: provider.exam.tests.length,
+                    padding: const EdgeInsets.symmetric(vertical: 4),
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) => Padding(
+                      padding: const EdgeInsets.only(left: 4.0),
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(4),
+                        onTap: () => provider.setTestByIndex(index),
+                        child: Ink(
+                          width: provider.currentTestIndex == index ? 90 : 60,
+                          decoration: BoxDecoration(
+                            color: provider.exam.tests[index].answerId != null ? Theme.of(context).colorScheme.secondary : Theme.of(context).colorScheme.primary,
+                            borderRadius: BorderRadius.circular(4),
+                            border: Border.all(width: 1, color: Theme.of(context).colorScheme.secondary),
+                          ),
+                          child: Center(
+                            child: Text(
+                              (index + 1).toString(),
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: provider.exam.tests[index].answerId != null ? Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.secondary,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
